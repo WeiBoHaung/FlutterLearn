d812c6f824f3bce88167fb561737955cf7ee15db
Web <web40243142@gmail.com>
2023-10-17 14:11:32 +0800

    ver1.0.0

---
+++ lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("WebHuang ID Card"),
+        backgroundColor: Colors.grey[850],
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/image01.jpg'),
+                radius: 40.0,
+              ),
+            ),
+            Divider(
+              height: 90.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "WeiBo Huang",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 30.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              "CURRENT WEB LEVEL",
+              style: TextStyle(
+                color: Colors.grey[800],
+                fontSize: 20.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              "8",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 30.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[200],
+                ),
+                Text(
+                  "web40243142@gmail.com",
+                  style: TextStyle(color: Colors.grey[200]),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
